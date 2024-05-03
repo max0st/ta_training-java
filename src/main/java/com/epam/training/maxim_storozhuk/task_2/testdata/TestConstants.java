006a49dfa1c13b07000cbc5d92e76dc3aff4a4bb
Your GitLab Username <maxstorozhuk4@gmail.com>
2024-05-03 14:24:02 +0300

    init

--- src/main/java/com/epam/training/maxim_storozhuk/task_2/testdata/TestConstants.java
+++ src/main/java/com/epam/training/maxim_storozhuk/task_2/testdata/TestConstants.java
@@ -3,7 +3,6 @@ package com.epam.training.maxim_storozhuk.task_2.testdata;
 public class TestConstants {
     public static final String CODE_SNIPPET = "            git config --global user.name  \"New Sheriff in Town\"\n" +
             "            git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\n" +
-            "            git push origin master --force\n" +
-            " ";
+            "            git push origin master --force";
     public static final String TITLE = "how to gain dominance among developers";
 }
